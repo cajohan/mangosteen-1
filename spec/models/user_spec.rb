fbf38fa27c1ff2f1d24a68b33382bd3c2c7582d4
john <huangdi_c@163.com>
2023-11-13 17:18:03 +0800

    perf: 删掉多余文件

--- spec/models/user_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
-  it 'have email' do   #
-    user = User.new email: 'john@1.com'
-    expect(user.email).to eq 'john@1.com'  # test code
-  end
-end
