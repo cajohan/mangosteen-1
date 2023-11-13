5919d1b1753687d9c4a37281246520d215afc2b5
john <huangdi_c@163.com>
2023-11-13 16:43:27 +0800

    refactor: 让 create validation_code 更简洁

--- app/models/validation_code.rb
+++ app/models/validation_code.rb
@@ -2,4 +2,14 @@ class ValidationCode < ApplicationRecord
   # has_secure_token :code, length: 24
   # email必填
   validates :email, presence: true
+  
+  after_initialize :generate_code
+  after_create :send_email
+
+  def generate_code
+    self.code = SecureRandom.random_number.to_s[2..7]
+  end
+  def send_email
+    UserMailer.welcome_email(self.email)
+  end
 end
