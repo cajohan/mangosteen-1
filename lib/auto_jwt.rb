f282028b11f05005d8ed4691435517657e938c9a
john <huangdi_c@163.com>
2023-11-14 11:42:52 +0800

    feat: 添加 AutoJwt 中间件

---
+++ lib/auto_jwt.rb
@@ -0,0 +1,16 @@
+class AutoJwt
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    # 状态码， 响应头， 响应体
+    # [200, {}, ["Hello, World!", "Hi!"]]
+    header = env['HTTP_AUTHORIZATION']
+    jwt = header.split(" ")[1] rescue ""
+    payload = JWT.decode jwt, Rails.application.credentials.hmac_secret, true, { algorithm: "HS256" } rescue nil
+    env['current_user_id'] = payload[0]['user_id'] rescue nil
+    @status, @headers, @response = @app.call(env)
+    [@status, @headers, @response]
+  end
+end
