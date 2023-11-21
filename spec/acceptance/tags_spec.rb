5d2f2d776eb655efbcdd47d8a8929b325392ba92
huangd <huangdi_c@163.com>
2023-11-21 15:24:25 +0800

    feat: 使用 create :user 代替原来代码

--- spec/acceptance/tags_spec.rb
+++ spec/acceptance/tags_spec.rb
@@ -3,17 +3,17 @@ require 'rspec_api_documentation/dsl'
 
 resource "标签" do
   authentication :basic, :auth
-  let(:current_user) { User.create email: '1@qq.com' }
+  let(:current_user) { create :user }
   let(:auth) { "Bearer #{current_user.generate_jwt}" }
   get "/api/v1/tags/:id" do
-    let (:tag) { Tag.create name: 'x', sign: 'x', user_id: current_user.id }
+    let (:tag) { create :tag, user: current_user }
     let (:id) { tag.id }
-    with_options :scope => :resource do 
-      response_field :id, 'ID' 
-      response_field :name, '名称' 
-      response_field :sign, '符号' 
-      response_field :user_id, '用户ID' 
-      response_field :deleted_at, '删除时间' 
+    with_options :scope => :resource do
+      response_field :id, 'ID'
+      response_field :name, "名称"
+      response_field :sign, "符号"
+      response_field :user_id, "用户ID"
+      response_field :deleted_at, "删除时间"
     end
     example "获取标签" do
       do_request
@@ -32,18 +32,18 @@ resource "标签" do
       response_field :user_id, "用户ID"
       response_field :deleted_at, "删除时间"
     end
-    
     example "获取标签列表" do
-      11.times do Tag.create name: 'x', sign:'x', user_id: current_user.id end
+      create_list :tag, Tag.default_per_page+1, user: current_user
       do_request
       expect(status).to eq 200
       json = JSON.parse response_body
-      expect(json['resources'].size).to eq 10
+      expect(json['resources'].size).to eq Tag.default_per_page
     end
   end
   post "/api/v1/tags" do
     parameter :name, '名称', required: true
     parameter :sign, '符号', required: true
+    parameter :kind, '类型', required: true, in: ['expenses', 'income']
     with_options :scope => :resource do
       response_field :id, 'ID'
       response_field :name, "名称"
@@ -53,6 +53,7 @@ resource "标签" do
     end
     let (:name) { 'x' }
     let (:sign) { 'x' }
+    let (:kind) { 'income' }
     example "创建标签" do
       do_request
       expect(status).to eq 200
@@ -62,7 +63,7 @@ resource "标签" do
     end
   end
   patch "/api/v1/tags/:id" do
-    let (:tag) { Tag.create name: 'x', sign:'x', user_id: current_user.id }
+    let (:tag) { create :tag, user: current_user }
     let (:id) { tag.id }
     parameter :name, '名称'
     parameter :sign, '符号'
@@ -82,11 +83,9 @@ resource "标签" do
       expect(json['resource']['name']).to eq name
       expect(json['resource']['sign']).to eq sign
     end
-
   end
-
   delete "/api/v1/tags/:id" do
-    let (:tag) { Tag.create name: 'x', sign: 'x', user_id: current_user.id }
+    let (:tag) { create :tag, user: current_user }
     let (:id) { tag.id }
     example "删除标签" do
       do_request
