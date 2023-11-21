06fe31273d0e3e363e198887e3240defac04c524
john <huangdi_c@163.com>
2023-11-21 15:59:27 +0800

    feat: item#index 接受 kind

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,7 +2,10 @@ class Api::V1::ItemsController < ApplicationController
   def index
     current_user_id = request.env['current_user_id']
     return head :unauthorized if current_user_id.nil?
-    items = Item.where(user_id: current_user_id).where({created_at: params[:created_after]..params[:created_before]}).page(params[:page])
+    items = Item.where(user_id: current_user_id)
+      .where(happen_at: params[:happen_after]..params[:happen_before])
+      .where(kind: params[:kind])
+      .page(params[:page])
     # items = Item.where("id>?",params[start_id]).limit(100) 如果是流形式内容，id需自增数字
     render json: { resources: items, pager: {
              page: params[:page] || 1,
